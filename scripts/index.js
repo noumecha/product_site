862145018b2d562d97d342d76a26045ba3bf79bf
noumecha <noumechaivan@gmail.com>
2022-01-05 18:03:20 +0100

    fin des molécules et début de l'orga du sie

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+
+var slideIndex = 0;
+showSlide();
+
+function showSlide() {
+
+    var i;
+    var slides = document.getElementsByClassName("theSlide");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i ++) {
+
+        slides[i].style.display = "none";
+
+    }
+    slideIndex ++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < slides.length; i ++) {
+
+        dots[i].className = dots[i].className.replace(" active", "");
+
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlide, 5000);
+
+}
